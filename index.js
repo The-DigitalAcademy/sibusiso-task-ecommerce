e50e1fcad817d1f3dcaebe97556fa5de95ca3577
unknown <hitprofit70@gmail.com>
2022-09-09 12:40:01 +0200

    card created in grid

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function cartOnScreen() {
     for (let i = 0; i < cartArray.length; i++) {
 
       cartItems.innerHTML += `
-      <div class="cartDisplay">
+      <div class="product1">
       <div class="product__img">
         <img
           src="${productsArray[i].image}"
@@ -167,8 +167,7 @@ function cartOnScreen() {
       <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
     </svg>
   </button>
-    </div>
-      `
+    </div>`
     }
 
     displayAmount.innerHTML = ""; 
